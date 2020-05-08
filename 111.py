2a72007c363eae32be08c967313dab55dc3e60fe
msw <321152108@qq.com>
2020-05-08 09:52:33 +0800

    111

---
+++ 111.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# plt.style.use('seaborn')  # 更换图像样式
+plt.rcParams['font.family'] = ['Arial Unicode MS', 'Microsoft Yahei', 'SimHei', 'sans-serif']  # 解决中文乱码
+# plt.rcParams['axes.unicode_minus'] = False  # 如果字体为SimHei黑体,负号乱码,解决
+
+# MAC电脑解决图片模糊魔术命令
+# %config InlineBackend.figure_format = 'retina'
